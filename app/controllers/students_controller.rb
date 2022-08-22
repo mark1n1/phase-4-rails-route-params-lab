323794f1680e730b32acd4217c2bd006e2d49c8e
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-08-22 11:28:12 -0700

    Completed lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,15 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if(params[:name])
+      students = Student.all.filter {|student| (student.first_name.downcase == params[:name] || student.last_name.downcase == params[:name]) ? student : nil}
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
